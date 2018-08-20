30d46aaaf6bb9879b2464f5d1c4fab31ee17e85a
Chris Franklin <cf73021@gmail.com>
2018-08-20 19:51:07 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+require 'pry'
+class Artist
+attr_accessor :name
+@@all_songs = []
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(title)
+    @songs << title
+  end
+
+  def songs
+    @songs
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(name)
+    if @@all.include?(name)
+      name
+    else
+      new_artist = Artist.new(name)
+      @@all << new_artist
+      new_artist
+    end
+  end
+
+  def print_songs
+    @songs.each do |x|
+      puts x.title
+    end
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initialize(title)
+    @title = title
+  end
+
+  def artist_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+  def self.new_by_filename(filename)
+    song_split = filename.split(" - ")
+    song = Song.new(song_split[1])
+    song.artist_name(song_split[0])
+    song
+  end
+end
